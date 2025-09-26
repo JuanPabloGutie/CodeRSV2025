91cc26826925b742177cb117e3d0ad3b3c26996d
Juan Pablo Gutiérrez <104776855+JuanPabloGutie@users.noreply.github.com>
2025-09-26 11:13:22 -0300

    Add files via upload

--- fR0VRS2.m
+++ fR0VRS2.m
@@ -7,26 +7,23 @@ bv1 bv2 bv3 bv4  deltav gv psiv ...
 phi phiv1 phiv2 phiv3 phiv4 ...
 N H  ...
 %N=1;
-%NG=x(1)+x(2)+x(3);%x(4) son removidos no cuentan como poblacion
-%NV=x(5)+x(6)+x(7)+x(8);
-%NC=x(10)+x(11);
-w=round(x(5)/H); %funcion parte entera
+w=round(x(5)/H); 
 %w=1;
 y=abs(sigma2-gH);
 %y=1;
-%No vacunados
+%Unvaccinated
 dx1 = Lambda - (b2*x(1)*(x(3)+x(9))./N)*(p./x(11)) - (phiv2+d)*x(1) + psi*x(4) + phi*x(7); %S
 dx2 = (b2*x(1)*(x(3)+x(9))./N)*(p./x(11)) - (delta+d)*x(2); %E
 dx3 = delta*x(2) - (sigma2 + g + d)*x(3); %I
 dx4 = g*x(3) + gH*x(5) - (psi + d)*x(4); %R
-%Hospitalizados
+%Hospitalized
 dx5 = sigma2*x(3) - (gH + dH)*x(5) - w*y*x(5)+ gH*x(6); %H 
 dx6 = w*y*x(5) - (gH+dH)*x(6); %Hc
-%Vacunados
+%Vaccinated
 dx7 = phiv2*x(1) - (bv2*x(7)*(x(3)+x(9))./N)*(p./x(11)) - (phi+d)*x(7) + psiv*x(10); %Sv
 dx8 = (bv2*x(7)*(x(3)+x(9))./N)*(p./x(11)) - (deltav + d)*x(8); %Ev 
 dx9 = deltav*x(8) - (gv + d)*x(9); %Iv
 dx10 = gv*x(9) - (psiv + d)*x(10); %Rv
-%Percepcion
+%Perception
 dx11 = -l1*(x(11)-p) + l2*(x(3)+x(5)+x(6)+x(9))./N; %p
 dx=[dx1;dx2;dx3;dx4;dx5;dx6;dx7;dx8;dx9;dx10;dx11];
